850132d108dc2c49bf38b07ee980d002d1b908bf
stevelucascs <stevelucascs@gmail.com>
2019-01-19 18:32:06 -0600

    models and srcaper added

---
+++ Scraper/API/utils/shineUrls.js
@@ -0,0 +1,72 @@
+export default ([
+  {
+    category: 'Hot Appetizers',
+    url:
+      'https://shineasian.carry-out.com/showmenuitems.asp?q1=101&q2=HOT%20APPETIZERS&q3=0&sid=0.5416997159718355'
+  },
+  {
+    category: 'Cold Appetizers',
+    url:
+      'https://shineasian.carry-out.com/showmenuitems.asp?q1=105&q2=STIR%20FRY&q3=0&sid=0.4334198495742805'
+  },
+  {
+    category: 'Soups',
+    url:
+      'https://shineasian.carry-out.com/showmenuitems.asp?q1=103&q2=SOUPS&q3=0&sid=0.24288758773394137'
+  },
+  {
+    category: 'Noodles & Rice',
+    url:
+      'https://shineasian.carry-out.com/showmenuitems.asp?q1=104&q2=NOODLES%20+%20RICE&q3=0&sid=0.9632435152712902'
+  },
+  {
+    category: 'Stir Fry',
+    url:
+      'https://shineasian.carry-out.com/showmenuitems.asp?q1=105&q2=STIR%20FRY&q3=0&sid=0.7681140956001788'
+  },
+  {
+    category: 'House Specials',
+    url:
+      'https://shineasian.carry-out.com/showmenuitems.asp?q1=106&q2=HOUSE%20SPECIALS&q3=0&sid=0.5255524761714898'
+  },
+  {
+    category: 'Sushi Dinners',
+    url:
+      'https://shineasian.carry-out.com/showmenuitems.asp?q1=107&q2=SUSHI%20DINNERS&q3=0&sid=0.5048389151134369'
+  },
+  {
+    category: 'Traditional Rolls',
+    url:
+      'https://shineasian.carry-out.com/showmenuitems.asp?q1=108&q2=TRADITIONAL%20ROLLS&q3=0&sid=0.3770469907642695'
+  },
+  {
+    category: 'Nigiri / Sashimi',
+    url:
+      'https://shineasian.carry-out.com/showmenuitems.asp?q1=109&q2=NIGIRI%20/%20SASHIMI&q3=0&sid=0.9922415165797509'
+  },
+  {
+    category: 'Signature Rolls',
+    url:
+      'https://shineasian.carry-out.com/showmenuitems.asp?q1=110&q2=SIGNATURE%20ROLLS&q3=0&sid=0.2626583416575854'
+  },
+  {
+    category: 'Staff Favorites',
+    url:
+      'https://shineasian.carry-out.com/showmenuitems.asp?q1=112&q2=STAFF%20FAVORITE&q3=0&sid=0.6405970290548078'
+  },
+  {
+    category: "Chef's Special Rolls",
+    url:
+      'https://shineasian.carry-out.com/showmenuitems.asp?q1=110&q2=SIGNATURE%20ROLLS&q3=0&sid=0.7347643380776605'
+  },
+  {
+    category: "Chef's Specials - Small Dishes",
+    url:
+      'https://shineasian.carry-out.com/showmenuitems.asp?q1=177&q2=CHEFS%20SPECIAL%20(SMALL%20DISHES)&q3=0&sid=0.7680090495771084'
+  },
+  {
+    category: "Chef's Specials - Big Plates",
+    url:
+      'https://shineasian.carry-out.com/showmenuitems.asp?q1=178&q2=CHEFS%20SPECIAL%20(BIG%20PLATE)&q3=0&sid=0.12753096585215928'
+  }
+]);
