2fb42cb50473c7f45aca785f9e82e6825972158b
Ryan BrodeFrank <ryanafrank@gmail.com>
2019-01-23 14:15:10 -0600

    pulling from master

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
-import Register from "./components/Register";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
+import RegisterRestaurant from "./components/RegisterRestaurant";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          Hello
-          <Register />
-        </header>
+      <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register/restaurant" component={RegisterRestaurant} />
+        {/* <Route exact path="/register/user" component={RegisterUser} /> */}
       </div>
+    </Router>
     );
   }
 }
