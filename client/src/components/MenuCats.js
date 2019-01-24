b9dba7acfd7ef1cade46383874b55fb37e430ad2
kat zhang <kzhang193@gmail.com>
2019-01-24 09:48:04 -0600

    I ALIGNED THE MENU BUTTONSSSS

--- client/src/components/MenuCats.js
+++ client/src/components/MenuCats.js
@@ -10,9 +10,11 @@ class MenuCats extends React.Component {
             box:{
                 backgroundColor: "red",
                 height: "100px",
-                width: "150px"
-        
-            }
+                width: "150px",
+                display: 'inline-block',
+    
+            },
+           
     }
     return (
       <div  style={myStyle.box}><h1> {this.props.category}</h1></div>
