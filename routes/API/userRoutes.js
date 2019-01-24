db20c48262f0e653fc84db18b6a6ff275bd3597e
Ryan BrodeFrank <ryanafrank@gmail.com>
2019-01-23 21:34:03 -0600

    users can now register

---
+++ routes/API/userRoutes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const userController = require("../../controllers/usersController");
+
+
+// Matches with "/api/users"
+router.route("/")
+  .get(userController.findAll)
+  .post(userController.create);
+
+// Matches with "/api/users/:id"
+router
+  .route("/:id")
+  .get(userController.findById)
+  .put(userController.update)
+  .delete(userController.remove);
+
+module.exports = router;
