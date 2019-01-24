3e3c36dadf656f83f1da7fdf0cee9c8b9f8a66ed
Andrea Minhas <andrea.minhas@gmail.com>
2019-01-23 22:03:54 -0600

    tables rendered and background color changes on click

--- client/src/pages/FloorPlan.js
+++ client/src/pages/FloorPlan.js
@@ -1,24 +1,76 @@
 import React from "react";
-import RoundTable from "../components/RoundTable";
-import SquareTable from "../components/SquareTable";
-import ServerStation from "../components/ServerStation";
+import Table from "../components/Tables";
 
 class FloorPlan extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  // constructor(props) {
+  //   super(props);
+
+  // }
+
+  state = {
+    tables: [
+      {
+        tableNumber: 1,
+        tableShape: "rounded-corner"
+      },
+      {
+        tableNumber: 2,
+        tableShape: "square"
+      },
+      {
+        tableNumber: 3,
+        tableShape: "square"
+      },
+      {
+        tableNumber: 4,
+        tableShape: "square"
+      },
+      {
+        tableNumber: 5,
+        tableShape: "square"
+      },
+      {
+        tableNumber: 6,
+        tableShape: "rounded-corner"
+      },
+      {
+        tableNumber: 7,
+        tableShape: "round"
+      },
+      {
+        tableNumber: 8,
+        tableShape: "round"
+      },
+      {
+        tableNumber: 9,
+        tableShape: "round"
+      },
+      {
+        tableNumber: 10,
+        tableShape: "round"
+      },
+      {
+        tableNumber: 11,
+        tableShape: "round"
+      },
+      {
+        tableNumber: 12,
+        tableShape: "round"
+      }
+    ]
+  };
 
   render() {
     return (
       <div>
-        <RoundTable tableNumber={1} shape={"round"} />
-        <RoundTable text={"2"} />
-        <RoundTable text={"3"} />
-        <SquareTable text={"4"} />
-        <SquareTable text={"5"} />
-
-        <ServerStation />
+        {this.state.tables.map(table => {
+          return (
+            <Table
+              tableNumber={table.tableNumber}
+              tableShape={table.tableShape}
+            />
+          );
+        })}
       </div>
     );
   }
