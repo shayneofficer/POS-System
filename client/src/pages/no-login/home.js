7a8fa6757f7b859afe48191b9b69813b9ddf4a77
Hannah Willis <han.e.willis@gmail.com>
2019-01-25 10:21:43 -0600

    reservation pages

--- client/src/pages/no-login/home.js
+++ client/src/pages/no-login/home.js
@@ -1,4 +1,7 @@
 import React from "react";
+import shine from "../../Images/shine.jpg"
+import shine2 from "../../Images/shine2.jpg"
+import shine3 from "../../Images/shine3.jpg"
 
 class home extends React.Component {
     constructor(props) {
@@ -10,7 +13,7 @@ class home extends React.Component {
         return (
             <div>
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="#">POS</a>
+                    <a class="navbar-brand" href="/">POS</a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
                         aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -29,15 +32,19 @@ class home extends React.Component {
                         <hr />
                         <h2 class="text-center">Book your seat at our exclusive POS restaurants!</h2>
                         <br />
+
                         <div class="text-center">
                             {/* <a href="/tables"><button class="btn btn-lg btn-primary"><span class="fa fa-list-alt"></span> View Tables</button></a> */}
                             <a href="/Reservations"><button class="btn btn-lg btn-warning"><span class="fa fa-credit-card"></span> Make
                   Reservation</button></a>
                         </div>
                     </div>
-                    {/* <br />
-                    <h3>POPULAR RESTAURANTS</h3> */}
-
+                    <div>
+                        <img src={shine} alt={"logo"} width="250px" height="250px"></img>
+                        <img src={shine2} alt={"logo"} width="250px" height="250px"></img>
+                        <img src={shine3} alt={"logo"} width="250px" height="250px"></img>
+                        <img src={shine3} alt={"logo"} width="250px" height="250px"></img>
+                    </div>
                 </div>
                 <footer class="footer">
                     {/* <div class="container">
@@ -45,7 +52,7 @@ class home extends React.Component {
                     </div> */}
                 </footer>
 
-            </div>
+            </div >
         );
     }
 }
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,18 +1,22 @@
 import axios from "axios";
 
 export default {
-  createRestaurant: function(data) {
+  createRestaurant: function (data) {
     return axios.post("/api/restaurants", data);
   },
-  getRestaurants: function() {
+  getRestaurants: function () {
     return axios.get("/api/restaurants");
   },
-  createUser: function(data) {
+  createUser: function (data) {
     console.log("createUser", data);
     return axios.post("/api/users", data);
   },
-  userLogin: function(data) {
+  userLogin: function (data) {
     console.log("userLogin", data);
     return axios.post("/api/users/login", data);
+  },
+  createReservation: function (data) {
+    console.log("createReservation", data);
+    return axios.post("/api/reservations")
   }
 };
