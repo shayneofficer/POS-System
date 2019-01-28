476272300faccbb9f099e9139cf0e8a226ad5c91
Ryan BrodeFrank <ryanafrank@gmail.com>
2019-01-28 14:19:35 -0600

    fixed login bug

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -43,9 +43,9 @@ class Login extends Component {
       <div>
         <form action="/action_page.php">
           <br />
-          <Input onChange={this.handleInputChange} type="text" placeholder="Email" />
+          <Input onChange={this.handleInputChange} type="text" name="email" placeholder="Email" />
           <br />
-          <Input onChange={this.handleInputChange} type="password" placeholder="Password" />
+          <Input onChange={this.handleInputChange} type="password" name="password" placeholder="Password" />
           <br />
           <FormBtn onClick={this.handleFormSubmit}>Submit</FormBtn>
         </form>
