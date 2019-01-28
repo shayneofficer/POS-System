9466cd44f8d079d3363078ea92cb153c27475354
kat zhang <kzhang193@gmail.com>
2019-01-28 14:04:47 -0600

    checking branches

--- client/src/pages/post-login/OrderInput.js
+++ client/src/pages/post-login/OrderInput.js
@@ -17,9 +17,9 @@ class Order extends React.Component {
       "Chef's Special Rolls",
       "House Specials"
     ],
-    tableNo: ["1", "2", "3", "4", "5", "7", "8", "9", "10", "11", "12"],
-    items: [],
-    menu: {}
+    // tableNo: ["1", "2", "3", "4", "5", "7", "8", "9", "10", "11", "12"],
+    // items: [],
+    // menu: {}
   };
   componentDidMount = () => {
     this.getMenu();
