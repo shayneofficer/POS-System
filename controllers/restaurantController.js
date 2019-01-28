1aadbc0391914f5756619e3f3967b98f5091f70e
stevelucascs <stevelucascs@gmail.com>
2019-01-28 13:50:02 -0600

    Controllers updated

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -2,36 +2,36 @@ const db = require('../models');
 
 // Defining methods for the POSController
 module.exports = {
-  findAll: function (req, res) {
+  find: function (req, res) {
     db.Restaurant
-      .find()
-      .then(results => res.json(results))
+      .find(req.query)
+      .then(restaurant => res.json(restaurant))
       .catch(err => res.status(422).json(err));
   },
   findById: function (req, res) {
     db.Restaurant
       .findById(req.params.id)
-      .then(results => res.json(results))
+      .then(restaurant => res.json(restaurant))
       .catch(err => res.status(422).json(err));
   },
   create: function (req, res) {
     //add validation for creation
     db.Restaurant
       .create(req.body)
-      .then(results => res.json(results))
+      .then(restaurant => res.json(restaurant))
       .catch(err => res.status(422).json(err));
   },
   update: function (req, res) {
     db.Restaurant
       .findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(results => res.json(results))
+      .then(restaurant => res.json(restaurant))
       .catch(err => res.status(422).json(err));
   },
   remove: function (req, res) {
     db.Restaurant
       .findById({ _id: req.params.id })
-      .then(results => results.remove())
-      .then(results => res.json(results))
+      .then(restaurant => restaurant.remove())
+      .then(restaurant => res.json(restaurant))
       .catch(err => res.status(422).json(err));
   }
 };
