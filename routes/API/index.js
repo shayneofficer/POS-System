f5f2f892197efb910160d5315b10aa82bc0bf9ae
stevelucascs <stevelucascs@gmail.com>
2019-01-28 10:54:47 -0600

    disabled routes temporarily

--- routes/API/index.js
+++ routes/API/index.js
@@ -1,12 +1,8 @@
 const router = require("express").Router();
 const restaurants = require('./restaurantRoutes');
-const menuRoutes = require('./restaurant/index');
 const users = require('./userRoutes');
-const reservations = require('./reservationsRoutes')
 
 // API routes
-router.use('/restaurants', restaurants);
-router.use('/menus', menuRoutes);
+router.use('/&rest=', restaurants);
 router.use('/users', users);
-router.use('/reservations', reservations);
 module.exports = router;
