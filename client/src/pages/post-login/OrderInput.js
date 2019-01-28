8f72e772c51e7a77261b710294ade73aa7bb2dfa
kat zhang <kzhang193@gmail.com>
2019-01-28 12:34:36 -0600

    went back to buttons

--- client/src/pages/post-login/OrderInput.js
+++ client/src/pages/post-login/OrderInput.js
@@ -17,7 +17,7 @@ class Order extends React.Component {
       "Chef's Special Rolls",
       "House Specials"
     ],
-    tableNo: [ "1", "2", "3", "4", "5", "7", "8", "9", "10", "11", "12" ],
+    tableNo: ["1", "2", "3", "4", "5", "7", "8", "9", "10", "11", "12"],
     items: [],
     menu: {}
   };
@@ -32,19 +32,21 @@ class Order extends React.Component {
       });
     });
   };
+
   getItems = (menuId) => {
     API.getMenuById(menuId).then((result) => {
       this.setState({ items: result.data.items });
       console.log(result);
     });
   };
+
   showItems = () => {
     // alert("YO LOOK AT THESE DISHES");
     console.log("AM I WORKING ");
     alert("hi");
   };
 
-  render () {
+  render() {
     const colSize = {
       row: {
         display: "flex"
@@ -77,22 +79,43 @@ class Order extends React.Component {
         borderColor: "grey",
         textAlign: "left",
         padding: "10px"
+      },
+
+      catbtn: {
+
+        width: "150px",
+        display: 'inline-block',
+        textAlign: "center",
+        paddingRight: "10px",
+        paddingLeft: "10px",
+        marginLeft: "5px",
+        marginTop: "5px"
       }
     };
+
+
     return (
       <div>
         <div className="row" style={colSize.row}>
           <div className="column" style={colSize.column}>
             <form>
-              Order for Table:
+              Order for :
               <select name="table-no">
-                <option value="table1">1</option>
-                <option value="table2">2</option>
-                <option value="table3">3</option>
-                <option value="table4">4</option>
+                <option value="table1">Table 1</option>
+                <option value="table2">Table 2</option>
+                <option value="table3">Table 3</option>
+                <option value="table4">Table 4</option>
+                <option value="table5">Table 5</option>
+                <option value="table6">Table 6</option>
+                <option value="table7">Table 7</option>
+                <option value="table8">Table 8</option>
+                <option value="table9">Table 9</option>
+                <option value="table10">Table 10</option>
+                <option value="table11">Table 11</option>
+                <option value="table12">Table 12</option>
               </select>
               <div style={colSize.ticket} className="orderList">
-                <h2>Check No. :</h2>
+
                 <table>
                   <tr>
                     <th>Item</th>
@@ -114,7 +137,17 @@ class Order extends React.Component {
                 <input type="submit" />
               </div>
             </form>
+
           </div>
+          <div className="column" style={colSize.column}>
+
+            {this.state.category.map((category) => {
+              return (<MenuCats category={category} showItems={this.showItems} />)
+            })}
+
+          </div>
+
+
           <hr />
         </div>
       </div>
