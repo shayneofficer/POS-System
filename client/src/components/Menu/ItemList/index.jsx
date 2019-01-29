0a8633c0df6acb5a5592613ce0ba8cf0f239e66e
kat zhang <kzhang193@gmail.com>
2019-01-29 10:18:00 -0600

    wont let me switch branches

---
+++ client/src/components/Menu/ItemList/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import List from "./List";
+import { Container, Row, Col } from "../../Grid";
+import "./List.css";
+
+class ItemList extends React.Component {
+  render () {
+    return (
+      <Container>
+        <Col size="sm-12">
+          {this.props.items.length ? (
+            <List key={this.props.key} children={this.props.items}>
+              {this.props.items.map((item, i) => {
+                return (
+                  <Row>
+                    <Col size="sm-9"><p onClick={this.props.orderItem} className="text-left">{item.name}</p></Col>
+                    <Col size="sm-3">{item.price}</Col>
+                  </Row>
+                );
+              })}
+            </List>
+          ) : (
+            <p>Select a Category to View Items</p>
+          )}
+        </Col>
+      </Container>
+    );
+  }
+}
+export default ItemList;
