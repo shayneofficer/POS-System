7c15b16b825679c7cac5f8bf05395d981d85efd9
Ryan BrodeFrank <ryanafrank@gmail.com>
2019-01-29 11:15:59 -0600

    fixing github merge issues

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -2,69 +2,41 @@ const db = require("../models");
 
 // Defining methods for the POSController
 module.exports = {
-  find: function (req, res) {
-    db.Restaurant
-      .findById(req.params.id)
-      .then((restaurant) => {
-        res.json(restaurant.Reservations.sort({ date: -1 }));
-      })
-      .catch((err) => res.status(422).json(err));
+  findAll: function (req, res) {
+    db.resList
+      .find({})
+      .sort({ date: -1 })
+      .then(results => res.json(results))
+      .catch(err => res.status(422).json(err));
   },
   findById: function (req, res) {
-    db.Restaurant
+    db.resList
       .findById(req.params.id)
-      .then((restaurant) =>
-        res.json(restaurant.Reservations.id(req.params.rsvId))
-      )
-      .catch((err) => res.status(422).json(err));
+      .then(results => res.json(results))
+      .catch(err => res.status(422).json(err));
   },
   create: function (req, res) {
-    db.Restaurant
-      .findById(req.params.id)
-      .then((restaurant) => {
-        restaurant.Reservations.push(req.body);
-        restaurant.save((err, restaurant) => {
-          if (err) res.status(422).json(err);
-          res.json(restaurant.Reservations);
-        });
-      })
-      .catch((err) => res.status(422).json(err));
+    console.log("Created resList", req.body);
+    db.resList
+      .create(req.body)
+      .then(results => res.json(results))
+      .catch(err => {
+        console.log("err", err);
+        res.status(422).json(err)
+      });
   },
   update: function (req, res) {
-    db.Restaurant
-      .findById(req.params.id)
-      .then((restaurant) => {
-        const reservations = restaurant.Reservations;
-        reservations.set(req.body);
-        return restaurant.save((err, restaurant) => {
-          if (err) res.status(422).json(err);
-          res.json(restaurant.Reservations);
-        });
-      })
-      .catch((err) => res.status(422).json(err));
-  },
-  updateOne: function (req, res) {
-    db.Restaurants.findById(req.params.id).then((restaurant) => {
-      const reservation = restaurant.Reservations.id(req.params.tableId)
-      reservation.set(req.body);
-      restaurant.save((err) => {
-        if (err) res.status(422).json(err);
-        res.json(restaurant.Reservations);
-      })
-    })
+    db.resList
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(results => res.json(results))
+      .catch(err => res.status(422).json(err));
   },
   remove: function (req, res) {
-    db.Restaurant
-      .findById(req.params.id)
-      .then((restaurant) => {
-        restaurant.Reservations
-          .id(req.params.rsvId)
-          .remove();
-        restaurant.save((err) => {
-          if (err) res.status(422).json(err);
-          res.json(restaurant);
-        });
-      })
-      .catch((err) => res.status(422).json(err));
-  }
+    db.resList
+      .findById({ _id: req.params.id })
+      .then(results => results.remove())
+      .then(results => res.json(results))
+      .catch(err => res.status(422).json(err));
+  },
+
 };
