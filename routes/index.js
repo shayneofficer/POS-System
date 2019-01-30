c5d6df20b368a030bfffd0199691a356e4d23e5c
Andrea Minhas <andrea.minhas@gmail.com>
2019-01-30 15:28:02 -0600

    seeing if heroku works

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
-const apiRoutes = require('./api');
+const apiRoutes = require("./API");
 const path = require("path");
 
 // const restaurantRoutes = require('./restaurants');
 // API Routes
 // router.use("/api", apiRoutes);
 // router.use('/api/restaurants', restaurantRoutes);
-router.use('/api', apiRoutes)
+router.use("/api", apiRoutes);
 
-router.use(function (req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
