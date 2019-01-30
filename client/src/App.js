36a521a522aa036607ee5b3e28accaca3dfdc2ad
Hannah Willis <han.e.willis@gmail.com>
2019-01-30 13:53:13 -0600

    switched images

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,10 @@ import FloorPlan from "./pages/post-login/FloorPlan";
 import Main from "./pages/no-login/Main";
 import Home from "./pages/post-login/Home";
 import OrderPage from "./pages/post-login/OrderPage/index";
-import ReservationList from "./pages/post-login/ReservationList";
-import Reservations from "./pages/no-login/Reservations";
+import ReservationList from "./components/reservationlist";
+import Reservations from "./pages/no-login/reservations";
 import NavBar from "./components/NavBar";
-import RestaurantHome from "./pages/no-login/RestaurantHome";
+import RestaurantHome from "./pages/no-login/restaurantHome";
 import ShineMenu from "./pages/post-login/PublicMenu/index";
 
 class App extends Component {
