b3310e81ea073603ce7320b261aaf1f147cf9322
Andrea Minhas <andrea.minhas@gmail.com>
2019-01-31 00:15:30 -0600

    working on reservation view

--- client/src/components/ServerKey.js
+++ client/src/components/ServerKey.js
@@ -3,7 +3,7 @@ import Table from "react-bootstrap/Table";
 
 const myStyles = {
   table: {
-    width: "20%",
+    width: "10%",
     marginLeft: "10px",
     fontSize: "1rem",
     fontWeight: "400",
@@ -17,26 +17,26 @@ class ServerKey extends React.Component {
       <Table style={myStyles.table} striped bordered hover>
         <thead>
           <tr style={{ backgroundColor: "lightgrey" }}>
-            <th colSpan="1">Server #</th>
-            <th colSpan="1">Server Name</th>
+            <th> Server #</th>
+            <th> Server Name</th>
           </tr>
         </thead>
         <tbody>
           <tr style={{ backgroundColor: "rebeccapurple", color: "white" }}>
-            <td colSpan="1">1</td>
-            <td colSpan="1">Jenny</td>
+            <td>1</td>
+            <td>Jenny</td>
           </tr>
           <tr style={{ backgroundColor: "darkblue", color: "white" }}>
-            <td colSpan="1">2</td>
-            <td colSpan="1">Tom</td>
+            <td>2</td>
+            <td>Tom</td>
           </tr>
           <tr style={{ backgroundColor: "darkgreen", color: "white" }}>
-            <td colSpan="1">3</td>
-            <td colSpan="1">Steve</td>
+            <td>3</td>
+            <td>Steve</td>
           </tr>
           <tr style={{ backgroundColor: "darkorange", color: "white" }}>
-            <td colSpan="1">4</td>
-            <td colSpan="1">Angie</td>
+            <td>4</td>
+            <td>Angie</td>
           </tr>
         </tbody>
       </Table>
