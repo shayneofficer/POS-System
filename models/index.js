f5742fe1b45cccc47cd9f7e14e70d50a4da5273c
Andrea Minhas <andrea.minhas@gmail.com>
2019-01-30 20:46:07 -0600

    bug fixing

--- models/index.js
+++ models/index.js
@@ -1,12 +1,11 @@
-
 module.exports = {
-  Reservation: require('./Schemas/reservations'),
-  Restaurant: require('./Schemas/Restaurants/Restaurant'),
-  Table: require('./Schemas/Restaurants/Table'),
-  Bill: require('./Schemas/Restaurants/Bill'),
-  Menu: require('./Schemas/Menus/Menu'),
-  MenuCategory: require('./Schemas/Menus/MenuCategory'),
-  MenuItem: require('./Schemas/Menus/MenuItem'),
-  Employee: require('./Schemas/Restaurants/Employee'),
+  Reservation: require("./Schemas/Reservations"),
+  Restaurant: require("./Schemas/Restaurants/Restaurant"),
+  Table: require("./Schemas/Restaurants/Table"),
+  Bill: require("./Schemas/Restaurants/Bill"),
+  Menu: require("./Schemas/Menus/Menu"),
+  MenuCategory: require("./Schemas/Menus/MenuCategory"),
+  MenuItem: require("./Schemas/Menus/MenuItem"),
+  Employee: require("./Schemas/Restaurants/Employee"),
   User: require("./user")
 };
