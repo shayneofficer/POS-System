e93642eec928561b58380ba0c4eb3c820ef8b4fe
Ryan BrodeFrank <ryanafrank@gmail.com>
2019-02-01 10:33:47 -0600

    fixed login/logout nav button... Note sessionStorage variables are all STRINGS!

--- client/src/components/NavBarButtons/LoginButton.js
+++ client/src/components/NavBarButtons/LoginButton.js
@@ -13,7 +13,7 @@ class LoginButton extends Component {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="/login" style={style.login}>Login<span className="sr-only">(current)</span></a>
+            <a className="nav-link" href="/login" style={style.login}>Log in<span className="sr-only">(current)</span></a>
           </li>
         </ul>
       </div>
